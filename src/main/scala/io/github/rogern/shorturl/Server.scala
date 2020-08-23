475370ea05c1368834e74ad196474bb5d6cd4a79
Roger Nordquist <roger.nordquist@gmail.com>
2020-08-23 21:24:35 +0200

    re-factored for testability and added route tests

--- src/main/scala/io/github/rogern/shorturl/Server.scala
+++ src/main/scala/io/github/rogern/shorturl/Server.scala
@@ -16,7 +16,37 @@ import scala.util.Try
 
 object Server extends App {
 
-  val redis = new RedisServer.Builder()
+  case class Payload(url: String)
+
+  class Api(urlService: UrlService) {
+
+    def healthcheck: Endpoint[IO, String] = get(pathEmpty) {
+      Ok("OK")
+    }
+
+    def create: Endpoint[IO, String] = post("create" :: jsonBody[Payload]) { p: Payload =>
+      for {
+        valid <- IO.fromTry(Try(URI.create(p.url)))
+        result <- urlService.createOrGet(valid)
+      } yield {
+        result match {
+          case Result.Existing(ShortUrl(url)) => Ok(url)
+          case Result.Created(ShortUrl(url)) => Created(url).withHeader("Location" -> url)
+        }
+      }
+    }
+
+    def lookup: Endpoint[IO, Unit] = get(path[String]) { path: String =>
+      urlService.get(ShortUrl(path)) map {
+        case Left(Missing) => Output.empty(Status.NotFound)
+        case Right(url) => Output.unit(Status.SeeOther).withHeader("Location" -> url.toASCIIString)
+      }
+    }
+
+    def run = healthcheck :+: create :+: lookup
+  }
+
+  private val redis = new RedisServer.Builder()
     .port(6379)
     .setting("bind 127.0.0.1")
     .build()
@@ -25,33 +55,8 @@ object Server extends App {
 
   private val urlService = new UrlService(new UrlRepo("redis://localhost:6379"))
 
-  case class Payload(url: String)
-
-  def healthcheck: Endpoint[IO, String] = get(pathEmpty) {
-    Ok("OK")
-  }
-
-  def create: Endpoint[IO, String] = post("create" :: jsonBody[Payload]) { p: Payload =>
-    for {
-      valid <- IO.fromTry(Try(URI.create(p.url)))
-      result <- urlService.createOrGet(valid)
-    } yield {
-      result match {
-        case Result.Existing(ShortUrl(url)) => Ok(url)
-        case Result.Created(ShortUrl(url)) => Created(url).withHeader("Location" -> url)
-      }
-    }
-  }
-
-  def lookup: Endpoint[IO, Unit] = get(path[String]) { path: String =>
-    urlService.get(ShortUrl(path)) map {
-      case Left(Missing) => Output.empty(Status.NotFound)
-      case Right(url) => Output.unit(Status.SeeOther).withHeader("Location" -> url.toASCIIString)
-    }
-  }
-
   def service: Service[Request, Response] = Bootstrap
-    .serve[Text.Plain](healthcheck :+: create :+: lookup)
+    .serve[Text.Plain](new Api(urlService).run)
     .toService
 
   Await.ready(Http.server.serve(":8081", service))
--- src/test/scala/io/github/rogern/shorturl/ServerTest.scala
+++ src/test/scala/io/github/rogern/shorturl/ServerTest.scala
@@ -1,18 +1,53 @@
 package io.github.rogern.shorturl
 
-import io.finch._
-import org.scalatest.flatspec._
+import java.net.URI
 
-class ServerTest extends AnyFlatSpec {
-//  test("healthcheck") {
-//    assert(Main.healthcheck(Input.get("/")).awaitValueUnsafe() == Some("OK"))
-//  }
-//
-//  test("helloWorld") {
-//    assert(Main.helloWorld(Input.get("/hello")).awaitValueUnsafe() == Some(Main.Message("World")))
-//  }
-//
-//  test("hello") {
-//    assert(Main.hello(Input.get("/hello/foo")).awaitValueUnsafe() == Some(Main.Message("foo")))
-//  }
+import com.twitter.finagle.{Status, http}
+import org.scalatest.flatspec._
+import io.circe.generic.auto._
+import io.finch._
+import io.finch.circe._
+import io.github.rogern.shorturl.Server.Payload
+import org.scalatest.{BeforeAndAfter, OneInstancePerTest}
+import redis.embedded.RedisServer
+
+class ServerTest extends AnyFlatSpec with OneInstancePerTest with BeforeAndAfter {
+
+  val redis = new RedisServer.Builder()
+    .port(6379)
+    .setting("bind 127.0.0.1")
+    .build()
+
+  val urlRepo = new UrlRepo("redis://localhost:6379")
+  val api = new Server.Api(new UrlService(urlRepo))
+
+  before { redis.start() }
+  after { redis.stop() }
+
+  "healthcheck" should "return Ok(200)" in {
+    val Some(result) = api.healthcheck(Input.get("/")).awaitOutputUnsafe()
+    assert(result.status === http.Status(200))
+  }
+
+  "create" should "return Created(201)" in {
+    val payload = Payload("http://example.com")
+    val Some(result) = api.create(Input.post("/create").withBody[Application.Json](payload)).awaitOutputUnsafe()
+    assert(result.status === http.Status(201))
+    assert(result.value.length > 0)
+  }
+
+  "create" should "return Ok(200) if already exists" in {
+    urlRepo.save(ShortUrl("aaaccc"), URI.create("http://example.com")).unsafeRunSync()
+    val payload = Payload("http://example.com")
+    val Some(result) = api.create(Input.post("/create").withBody[Application.Json](payload)).awaitOutputUnsafe()
+    assert(result.status === http.Status(200))
+    assert(result.value === "aaaccc")
+  }
+
+  "get" should "return SeeOther(303) redirect to real url" in {
+    urlRepo.save(ShortUrl("abcdef"), URI.create("http://example.com")).unsafeRunSync()
+    val Some(result) = api.lookup(Input.get("/abcdef")).awaitOutputUnsafe()
+    assert(result.status === http.Status(303))
+    assert(result.headers.get("Location") === Some("http://example.com"))
+  }
 }
